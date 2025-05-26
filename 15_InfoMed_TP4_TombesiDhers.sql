fcf5ebe78a24e047687c5f919e6674a4845117e7
Rosario Tombesi <rotombesii@gmail.com>
2025-05-26 14:09:24 -0300

    Guardar cambios

--- 15_InfoMed_TP4_TombesiDhers.sql
+++
@@ -1,8 +0,0 @@
-SELECT p.nombre, c.fecha, c.diagnostico
-FROM Consultas c
-JOIN Pacientes p ON c.id_paciente = p.id_paciente
-WHERE c.fecha = (
-    SELECT MAX(c2.fecha)
-    FROM Consultas c2
-    WHERE c2.id_paciente = c.id_paciente
-);
